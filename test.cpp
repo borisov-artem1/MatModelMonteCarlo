c4f0e368431c7ceb7736c1a9825b1663337a6cdc
alex <tymin.a@bk.ru>
2024-05-30 21:41:13 +0300

    feat: function test Created

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@
 extern QVector<Wall*> vector;
 static Generator generator;
 
-void test()
+void test1()
 {
     Сylinder* cylinder = new Сylinder(30, 60);
     vector.push_back(cylinder);
@@ -16,5 +16,30 @@ void test()
     rand.gamma = 15;
     rand.teta = 15;
     NewCoordinates = rand;
-    generator.FlightMoleculeCylinder(NewCoordinates);
+    generator.FlightMoleculeCylinder(NewCoordinates,1);//передаем параметры такие как я указал на карте Miro , просмотри
+                                                       // заведомо известно что цилиндр обладает индексом 1, т.к. индексом 0 и 2 обладают
+                                                       // автоматически созданные диски с portal == true
 }
+
+void testForLocation()
+{
+
+}
+
+void test2()
+{
+    Сylinder* cylinder = new Сylinder(30, 60);
+    vector.push_back(cylinder);
+    generator.CreatingPortal();
+    Coordinates NewCoordinates;
+    RandomValues rand;
+    rand.height = 15;
+    rand.fi = 30;
+    rand.gamma = 15;
+    rand.teta = 15;
+    NewCoordinates = rand;
+    generator.FlightMoleculeCylinder(NewCoordinates,1);//передаем параметры такие как я указал на карте Miro , просмотри
+                                                       // заведомо известно что цилиндр обладает индексом 1, т.к. индексом 0 и 2 обладают
+                                                       // автоматически созданные диски с portal == true
+}
+
