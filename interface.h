ae25a8130a7433ded4dc8266bb6c72d431160412
Artem Borisov <artem.borisov2002@mail.ru>
2024-07-01 23:08:43 +0300

    fix: lexic problems

--- interface.h
+++ interface.h
@@ -24,7 +24,7 @@ public:
     double DispCalc(QVector<double>& XiVec, QVector<double>& DispVec, int molecules, int iteration);
     void checkingReproducibility(const QVector<double>& DispVec);
     QVector<double> polinomialCoefCalc(const QVector<double>& avgVec);
-    int significanceOfTheCoefficients(const QVector<double>& polinomCoef, double& ReproducibilityDisp,const QVector<double>& DispVec);
+    int significanceOfTheCoefficients(const QVector<double>& polinomCoef, double& ReproducibilityDisp, const QVector<double>& DispVec);
     void adequacyOfTheModel(int alphaSig, const QVector<double>& polinomCoef, const double& ReproducibilityDisp, const QVector<double>& avgVec);
 
 private:
